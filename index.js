14478527451cafe18e1dd7388d11650fa8000acb
Sean D <sdoherty8@icloud.com>
2017-11-12 20:03:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file!
-const companyName = "Scuber";
-let mostProfitableNeighborhood = "Chelsea";
-let companyCeo - "Susan Smith";
++ const companyName = "Scuber";
+
++ let mostProfitableNeighborhood = "Chelsea";
+
++ let companyCeo - "Susan Smith";
